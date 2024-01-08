ad0b811c9c27d6294a7027e123954810e72187d8
David Lidebrandt <david.lidebrandt@gmail.com>
2024-01-08 14:03:05 +0100

    Add logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import logo from "../public/images/logo-color.png"
 
 const Header = () => {
-  return (
-    <header>Header</header>
-  )
-}
+	return (
+		<header className="medium-blue-bg">
 
-export default Header
+			<nav>
+				<h1>
+					<Image
+						src={logo}
+						width={200}
+						height={100}
+						alt="Picture of the author"
+                        priority={true}
+					/>
+				</h1>
+				<ul></ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
