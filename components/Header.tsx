5469ac9f8b8daa2ef936c26b2cf9c02a4c0a7816
David Lidebrandt <david.lidebrandt@gmail.com>
2024-01-11 00:29:36 +0100

    Add nav links and button

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,19 +4,28 @@ import logo from "../public/images/logo-color.png"
 
 const Header = () => {
 	return (
-		<header className="medium-blue-bg">
+		<header className="medium-blue-bg p-6">
 
 			<nav>
-				<h1>
+				<h1 className="inline-block mb-3">
 					<Image
+						className="rounded"
 						src={logo}
-						width={200}
-						height={100}
-						alt="Picture of the author"
+						width={150}
+						height={25}
+						alt="Logo"
                         priority={true}
 					/>
 				</h1>
-				<ul></ul>
+				<button className="float-right align-bottom">
+					<div className="w-3 h-1 light-bg mb-2"></div>
+					<div className="w-5 h-1 light-bg mb-2"></div>
+					<div className="w-5 h-1 light-bg"></div>
+				</button>
+				<ul>Book Now</ul>
+				<ul>About Us</ul>
+				<ul>Register Company</ul>
+				<ul>Log In</ul>
 			</nav>
 		</header>
 	);
