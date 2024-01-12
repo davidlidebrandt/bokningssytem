15c61856411678d7a478ccb2242cffeb929c7fd7
David Lidebrandt <david.lidebrandt@gmail.com>
2024-01-12 17:42:16 +0100

    Add hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import Image from "next/image";
+import heroImage from "../public/images/calendar-cropped.jpg";
 
 export default function Home() {
-  return (
-    <main>Hello</main>  
-  )
+	return (
+		<main className="grid justify-items-center">
+			<section className="max-w-6xl">
+        <Image src={heroImage} alt="hero image"></Image>
+      </section>
+			<section className="max-w-6xl"></section>
+			<section className="max-w-6xl"></section>
+			<section className="max-w-6xl"></section>
+		</main>
+	);
 }
