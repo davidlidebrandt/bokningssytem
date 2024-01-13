d99f9963c8d10bbd258927c5cde5b122da448576
David Lidebrandt <david.lidebrandt@gmail.com>
2024-01-13 15:56:38 +0100

    Fix responsiveness of input fields

--- pages/register.tsx
+++ pages/register.tsx
@@ -5,50 +5,50 @@ function register() {
 	return (
 		<main className="light-blue-bg">
 			<section className="grid place-items-center w-full">
-				<div className="medium-blue-bg w-11/12 my-10 rounded-md grid place-items-center light-c">
-					<h2 className="my-6 font-bold text-xl">
+				<div className="medium-blue-bg w-11/12 my-16 rounded-md grid place-items-center light-c md:max-w-3xl">
+					<h2 className="my-10 font-bold text-xl">
 						Register your company
 					</h2>
-					<form className="text-sm w-full grid place-items-center">
-						<div className="w-4/5">
-							<label className="block" htmlFor="companyName">
+					<form className="text-sm w-full grid place-items-center md:w-4/5">
+						<div className="w-4/5 md:flex gap-3 md:mb-2">
+							<label className="block text-nowrap min-w-28" htmlFor="companyName">
 								Company name
 							</label>
 							<input
-								className="block mb-3 rounded-lg w-full text-lg dark-blue-c"
+								className="block mb-3 rounded-lg w-full text-lg dark-blue-c md:w-auto md:grow"
 								id="companyName"
 								name="companyName"
 								type="text"
 							/>
 						</div>
-						<div className="w-4/5">
-							<label className="block" htmlFor="companyName">
-								Company name
+						<div className="w-4/5 md:flex gap-3 md:mb-2">
+							<label className="block text-nowrap min-w-28" htmlFor="email">
+								Email
 							</label>
 							<input
-								className="block mb-3 rounded-lg w-full text-lg dark-blue-c"
-								id="companyName"
-								name="companyName"
+								className="block mb-3 rounded-lg w-full text-lg dark-blue-c md:w-auto md:grow"
+								id="email"
+								name="email"
 								type="text"
 							/>
 						</div>
-						<div className="w-4/5">
-							<label className="block" htmlFor="password">
+						<div className="w-4/5 md:flex gap-3 md:mb-2">
+							<label className="block text-nowrap min-w-28" htmlFor="password">
 								Password
 							</label>
 							<input
-								className="block mb-3 rounded-lg w-full text-lg dark-blue-c"
+								className="block mb-3 rounded-lg w-full text-lg dark-blue-c md:w-auto md:grow"
 								id="password"
 								name="password"
 								type="text"
 							/>
 						</div>
-						<div className="w-4/5">
-							<label className="block" htmlFor="confirmPassword">
+						<div className="w-4/5 md:flex gap-3">
+							<label className="block text-nowrap min-w-28" htmlFor="confirmPassword">
 								Confirm password
 							</label>
 							<input
-								className="block mb-5 rounded-lg w-full text-lg dark-blue-c"
+								className="block mb-5 rounded-lg w-full text-lg dark-blue-c md:w-auto md:grow"
 								id="confirmPassword"
 								name="confirmPassword"
 								type="text"
